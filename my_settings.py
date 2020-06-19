f946a30c62b84c216599cbab57c76a1f9dc956f0
SeongwonMoon <swmarkmoon@gmail.com>
2020-06-19 14:43:54 +0900

    add gitignore file and set security file and db updated from sqlite to mysql

---
+++ my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westagram',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET = {
+        'secret':'&2sfca(9tpr!d#hhgci=jwy)&wz0q78!dx$@7)4ytel!)b6$#^',
+}
